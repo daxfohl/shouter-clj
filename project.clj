f3369158eb85fc3337479c5249eedd30659ba420
Dax Fohl <dax.fohl@gmail.com>
2012-05-15 18:52:37 -0700

    init

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject shouter "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [postgresql/postgresql "9.1-901.jdbc4"]
+                 [org.clojure/java.jdbc "0.2.1"]
+                 [ring/ring-jetty-adapter "1.1.0"]
+                 [compojure "1.0.4"]
+                 [hiccup "1.0.0"]])
